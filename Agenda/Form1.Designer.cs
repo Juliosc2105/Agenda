ed02f26126dd7bce5e0f76133012dc7ba878839e
Julio Cardoso <juliocesarsc2008@gmail.com>
2021-09-16 12:30:56 -0300

    Adicionando os códigos

--- Agenda/Form1.Designer.cs
+++ Agenda/Form1.Designer.cs
@@ -31,21 +31,21 @@ namespace Agenda
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
+            this.btbAdicionar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtFone = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbxSexo = new System.Windows.Forms.ComboBox();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.lstAgenda = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -67,17 +67,18 @@ namespace Agenda
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(135, 53);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Limpar Campos";
+            this.button1.Text = "Atualizar";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnExcluir
             // 
-            this.button2.Location = new System.Drawing.Point(153, 385);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 53);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Excluir";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnExcluir.Location = new System.Drawing.Point(153, 385);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(135, 53);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
             // button3
             // 
@@ -106,31 +107,25 @@ namespace Agenda
             this.button5.Text = "Limpar Agenda";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // btbAdicionar
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(294, 84);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(416, 290);
-            this.listBox1.TabIndex = 6;
+            this.btbAdicionar.Location = new System.Drawing.Point(153, 321);
+            this.btbAdicionar.Name = "btbAdicionar";
+            this.btbAdicionar.Size = new System.Drawing.Size(135, 53);
+            this.btbAdicionar.TabIndex = 8;
+            this.btbAdicionar.Text = "Adicionar";
+            this.btbAdicionar.UseVisualStyleBackColor = true;
+            this.btbAdicionar.Click += new System.EventHandler(this.btbAdicionar_Click);
             // 
-            // button6
+            // btnNovo
             // 
-            this.button6.Location = new System.Drawing.Point(153, 321);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(135, 53);
-            this.button6.TabIndex = 8;
-            this.button6.Text = "Editar";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(12, 321);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(135, 53);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "Salvar Registro";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnNovo.Location = new System.Drawing.Point(12, 321);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(135, 53);
+            this.btnNovo.TabIndex = 7;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
             // 
             // label2
             // 
@@ -142,21 +137,21 @@ namespace Agenda
             this.label2.TabIndex = 9;
             this.label2.Text = "Nome";
             // 
-            // textBox1
+            // txtNome
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 103);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(276, 26);
-            this.textBox1.TabIndex = 10;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(12, 103);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(276, 26);
+            this.txtNome.TabIndex = 10;
             // 
-            // textBox2
+            // txtFone
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(12, 184);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(276, 26);
-            this.textBox2.TabIndex = 12;
+            this.txtFone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFone.Location = new System.Drawing.Point(12, 184);
+            this.txtFone.Name = "txtFone";
+            this.txtFone.Size = new System.Drawing.Size(276, 26);
+            this.txtFone.TabIndex = 12;
             // 
             // label3
             // 
@@ -178,14 +173,18 @@ namespace Agenda
             this.label4.TabIndex = 13;
             this.label4.Text = "Sexo";
             // 
-            // comboBox1
+            // cbxSexo
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 262);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(276, 28);
-            this.comboBox1.TabIndex = 14;
+            this.cbxSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSexo.FormattingEnabled = true;
+            this.cbxSexo.Items.AddRange(new object[] {
+            "",
+            "Masculino ",
+            "Feminino"});
+            this.cbxSexo.Location = new System.Drawing.Point(12, 262);
+            this.cbxSexo.Name = "cbxSexo";
+            this.cbxSexo.Size = new System.Drawing.Size(276, 28);
+            this.cbxSexo.TabIndex = 14;
             // 
             // button8
             // 
@@ -213,26 +212,36 @@ namespace Agenda
             this.button9.Text = "Fechar";
             this.button9.UseVisualStyleBackColor = false;
             // 
+            // lstAgenda
+            // 
+            this.lstAgenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstAgenda.FormattingEnabled = true;
+            this.lstAgenda.ItemHeight = 25;
+            this.lstAgenda.Location = new System.Drawing.Point(294, 84);
+            this.lstAgenda.Name = "lstAgenda";
+            this.lstAgenda.Size = new System.Drawing.Size(417, 279);
+            this.lstAgenda.TabIndex = 17;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(722, 450);
+            this.Controls.Add(this.lstAgenda);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbxSexo);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtFone);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btbAdicionar);
+            this.Controls.Add(this.btnNovo);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -249,21 +258,21 @@ namespace Agenda
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExcluir;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button btbAdicionar;
+        private System.Windows.Forms.Button btnNovo;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtFone;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbxSexo;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ListBox lstAgenda;
     }
 }
 
--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -16,5 +16,71 @@ namespace Agenda
         {
             InitializeComponent();
         }
+
+        private void btbAdicionar_Click(object sender, EventArgs e)
+        {
+            if ((txtNome.Text != "" ) && (txtFone.Text != "" ) && (cbxSexo.Text != "" ))
+            {
+                if (MessageBox.Show(
+                    "Deseja incluir esses dados na lista?",
+                    "Informação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    lstAgenda.Items.Add(txtNome.Text + ";" + txtFone.Text + ";" + cbxSexo.Text);
+                    LimparCampos();
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Preencha todos os campos",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(
+                "Deseja limpar todos os campos?",
+                "Informação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                LimparCampos();
+            }
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Clear();
+            txtFone.Clear();
+            cbxSexo.Text = "";
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (lstAgenda.SelectedIndex >= 0 )
+            {
+                if(MessageBox.Show(
+                    "Deseja realmente excluir o item selecionado?",
+                    "Informação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    lstAgenda.Items.RemoveAt(lstAgenda.SelectedIndex);
+                }
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Selecione um registro na lista",
+                    "Informação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
     }
 }
